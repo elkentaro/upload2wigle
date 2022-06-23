367b6bd1f9450698086f5c4906cf023989c9609a
elkentaro <el.kentaro@gmail.com>
2022-06-23 12:02:53 +0900

    Update upload2wigle.sh

--- upload2wigle.sh
+++ upload2wigle.sh
@@ -4,6 +4,6 @@ echo $1
 
 APIName=[FILL IN YOUR API NAME]
 APIToken=[FILL IN YOUR API TOKEN]
-curl -X POST "https://api.wigle.net/api/v2/file/upload" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@$file;type=text/csv" -F "donate=on" -i -H 'Accept:application/json' -u $APIName:$APIToken --basic && mv $1 /kismets/toWigle/
+curl -X POST "https://api.wigle.net/api/v2/file/upload" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@$file;type=text/csv" -F "donate=on" -i -H 'Accept:application/json' -u $APIName:$APIToken --basic 
 
 
